020383182b1205d3b75467519c5eebc605fc397b
supham <supalpuket@gmail.com>
2020-03-30 11:33:08 +0700

    merge upstream/master

--- tmp/config.js
+++ tmp/config.js
@@ -1,1 +1,29 @@
-export default {}
+export default {
+  "pages": "/media/xubuncup/ntfsd/cup/www/00-digitak/digitrax/src/pages",
+  "sourceDir": "src",
+  "routifyDir": "node_modules/@sveltech/routify/tmp",
+  "ignore": [],
+  "unknownPropWarnings": true,
+  "dynamicImports": true,
+  "singleBuild": false,
+  "scroll": "smooth",
+  "extensions": [
+    "html",
+    "svelte",
+    "md"
+  ],
+  "distDir": "dist",
+  "noPrerender": false,
+  "ssr": true,
+  "prerender": true,
+  "staticDir": "static",
+  "scriptsDir": "scripts",
+  "childProcess": "rollup -c -w",
+  "unusedPropWarnings": true,
+  "baseUrl": "/nodejs/digitrax/dist",
+  "spaEntry": "/index.php"
+}
+
+export const baseUrl = "/nodejs/digitrax/dist"
+
+export const spaEntry = "/index.php"
